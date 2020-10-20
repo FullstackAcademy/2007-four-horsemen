63b6dcbbb26bb1a01c9e75f0d7de95e524964243
qinben10 <qinben10@gmail.com>
2020-10-20 16:41:31 -0400

    init store and components

---
+++ client/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import {Link} from 'react-router-dom'
+
+export default class Login extends React.Component {
+  
+    render() {
+      return <div>
+        <nav>Here is Login page</nav>
+        </div>
+  }
+  }
