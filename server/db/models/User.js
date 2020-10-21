ef0aec9fdfa2cb76269bb90fc9e8a36951c0e57d
Oscar Sanchez <oscarsanchez@Oscars-MacBook-Pro.local>
2020-10-21 16:58:26 -0500

    changed a couple of field names in models/users and models/products, seeding succeded for those two models and I adjusted init func in server/main.js

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -3,14 +3,13 @@ const { STRING, BOOLEAN } = Sequelize;
 const db = require('../conn');
 
 const User = db.define('user', {
-  username: {
+  name: {
     type: STRING,
     allowNull: false,
     validate: {
       notEmpty: true,
     },
   },
-
   //we would need to use bcrypt later to hash the userpassword in our db
   password: {
     type: STRING,
