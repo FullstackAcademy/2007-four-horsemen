bd271a93cad78e78e0a4e1090f1313fa62150843
Dmitry Akulov <36012593+DAKUL9830@users.noreply.github.com>
2020-10-23 17:33:26 -0400

    Update Models.js

--- client/components/Models.js
+++ client/components/Models.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import Model from './Model'
 
-export default class Models extends React.Component {
-  render() {
-    return (
-      <div>
-        <nav>Here is Models page</nav>
-      </div>
-    );
+class Models extends React.Component {
+  constructor(props){
+    super(props)
   }
-}
+    render() {
+      const {products}=this.props;
+      console.log(this.props)
+
+return (
+  <div>
+      {products.length?
+          <div >
+              
+              {
+                  products.map(product => {
+                      return(
+                          <Model  key={product.id} product={product}/>
+                      )
+                      })
+              }
+             
+          </div>
+          :<div>
+          <h3>DataBase is empty</h3>
+        </div>
+          }                                     
+  </div>
+)
+  }
+  }
+
+  const mapStateToProps = ({products}) => {
+    return {products};
+  };
+  
+  
+  
+  export default connect(mapStateToProps)(Models);
