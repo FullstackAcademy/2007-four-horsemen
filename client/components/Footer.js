9fb52544911e8dc6030207b79d11ced9b353aeca
Oscar Sanchez <00oscarsanchez00@gmail.com>
2020-10-23 19:04:50 -0500

    added footer

---
+++ client/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default Footer = () => {
+  return (
+    <div className="footer">
+      <footer>
+        <p> Copyright © 2020 Oscar Qinwei Laziz Dzimitry </p>
+      </footer>
+    </div>
+  );
+};
