b403308c814e9023674398f1291e5f22745866e6
Laziz <laziz.zokir26@gmail.com>
2020-10-25 02:01:29 -0400

    updated db models & associations

--- client/components/Models.js
+++ client/components/Models.js
@@ -1,54 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {Link} from 'react-router-dom';
-
-import {fetchProducts} from '../store/redux/products'
-import Model from './Model'
-
+import { Link } from 'react-router-dom';
+import { fetchProducts } from '../store/redux/products';
+import Model from './Model';
 
 class Models extends React.Component {
-
-  componentDidMount(){
-    this.props.getProducts()
+  componentDidMount() {
+    this.props.getProducts();
   }
-  
-    render() {
-      console.log(this.props.products)
 
-return (
-  <div>
-      {this.props.products.length?
-        <div > 
-              {
-                  this.props.products.map(product => {
-                      return(
-                        <Link to= {`/models/${product.id}`}>
-                          <Model  key={product.id} product={product}/>
-                        </Link>
-                      )
-                      })
-              }
-             
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        {this.props.products.length ? (
+          <div>
+            {this.props.products.map((product) => {
+              return (
+                <Link to={`/models/${product.id}`}>
+                  <Model key={product.id} product={product} />
+                </Link>
+              );
+            })}
           </div>
-          :<div>
-          <h3>No Car</h3>
-        </div>
-          }                                     
-  </div>
-)
+        ) : (
+          <div>
+            <h3>No Car</h3>
+          </div>
+        )}
+      </div>
+    );
   }
-  }
+}
 
-  const mapStateToProps = ({products}) => {
-    return {products};
+const mapStateToProps = ({ products }) => {
+  return { products };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getProducts: () => dispatch(fetchProducts()),
   };
-  const mapDispatchToProps = (dispatch) => {
-    return {
-      getProducts: ()=> dispatch(fetchProducts()),
-    };
-  };
-  
-  
-  
-  export default connect(mapStateToProps,mapDispatchToProps)(Models);
+};
 
+export default connect(mapStateToProps, mapDispatchToProps)(Models);
--- server/db/index.js
+++ server/db/index.js
@@ -1,49 +1,53 @@
 const db = require('./conn');
+const Cart = require('./models/Cart');
+const Order = require('./models/Order');
+const Payment = require('./models/Payment');
+const Product = require('./models/Product');
+const Shipment = require('./models/Shipment');
+const User = require('./models/User');
+const Review = require('./models/Review');
 
-
-const Cart = require('./models/Cart')
-const Customer = require('./models/Customer')
-const Order = require('./models/Order')
-const Payment = require('./models/Payment')
-const Product = require('./models/Product')
-const Shipment = require('./models/Shipment')
-const User = require('./models/User')
-const OrderItem = require('./models/OrderItem')
-
-/////customer view:
+//users
 User.hasMany(Order);
 User.hasOne(Cart);
 Cart.belongsTo(User);
 
-
-
-////order view:
+//orders
 Order.belongsTo(User);
 Order.hasMany(Product);
 Product.belongsTo(Order);
 
-Order.hasMany(OrderItem);
-OrderItem.belongsTo(Order);
+//carts
+Order.hasOne(Cart);
+Cart.belongsTo(Product);
 
-Product.hasMany(OrderItem);
-OrderItem.belongsTo(Product);
-
-Order.hasOne(Shipment)
+//shipments
+Order.hasOne(Shipment);
 Shipment.belongsTo(Order);
+
+//payments
 Order.hasMany(Payment);
 Payment.belongsTo(Order);
 
+//reviews
+User.hasMany(Review);
+Review.belongsTo(User);
+Product.hasMany(Review);
+Review.belongsTo(Product);
+
+//ratings
+Product.belongsToMany(Review, { through: 'ratings' });
+Review.belongsToMany(Product, { through: 'ratings' });
 
 module.exports = {
   db,
   models: {
     Cart,
-    Customer,
     Order,
-    OrderItem,
     Payment,
     Product,
     Shipment,
     User,
+    Review,
   },
 };
